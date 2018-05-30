9f56b9c8a1ac6949d7fb2a222de94946248c260f
Ruben Alliet <allietruben@gmail.com>
2018-05-30 19:21:32 +0200

    data cleanup

--- extractData.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://randomuser.me/api/?results=10'
-users_json = requests.get(url).json()
-url2 = 'https://jsonplaceholder.typicode.com/posts/'
-uploads_json = requests.get(url2).json()
